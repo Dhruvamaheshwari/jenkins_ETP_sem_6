64b141974bbca42c13b08ae1f1ee87f4d4c11930
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-26 19:46:21 +0530

    add line in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 4000;
 
 
 app.get('/' , (req  ,res)=>{
-   res.send('or ram kya hal h?? me mast hu tum batao')
+   res.send('or ram kya hal h?? me mast hu tum batao are are bs bs etp aane bale h')
 })
 
 app.listen(port , () => console.log(`server is listin on port ${port}`))
