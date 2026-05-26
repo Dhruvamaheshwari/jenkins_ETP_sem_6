f625b442ed84d58681a80001f2058cbf490306e1
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-26 10:29:56 +0530

    update the Docker file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
-const port = 5000;
+const app =  express();
+const port = 4000;
 
-app.get('/' , (req  ,res) => {
-    res.send('kyaa haal h jee , sb mast ??')
+
+app.get('/' , (req  ,res)=>{
+   res.send('Hello jee kya haal chaal')
 })
 
-
-app.listen(port , () => console.log(`server is started at port ${port}`))
+app.listen(port , () => console.log(`server is listin on port ${port}`))
