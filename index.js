2da4d6ae78fa93eec88031dd917ac146ef76c1f1
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-26 22:55:53 +0530

    add 2 min. time and add line in index.js

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const port = 4000;
 
 app.get('/' , (req  ,res)=>{
    res.send(`or ram kya hal h?? me mast hu tum batao are are bs bs etp aane bale h
+            <br/> <br/>
          <a href="/done">Done</a>`)
 })
 
 app.get('/done' , (req , res)=>{
-   res.send('ho gya paper devops ka')
+   res.send('ho gya paper devops ka , aab MVC ki prepration krni h')
 })
 
 app.listen(port , () => console.log(`server is listin on port ${port}`))
